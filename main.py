cc2bb5761c0b3e7c076406c1aa8bdfe20b90701e
Jiawei Liu <jliu259@binghamton.edu>
2021-06-21 10:54:29 -0400

    Milestone 2

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-#import your controller
+#import pygame
 
 def main():
+	pygame.init()
+	team = {"lead":"Jiawei/     ", "backend": "Jiawei Liu", "frontend":
+	print("Software Lead is: Jiawei/ ", team["lead"]
+	print("Backend is: Jiawei Liu", team["backend"]
+	print("Frontend is: ", team["frontend"]
     #Create an instance on your controller object
 main()
