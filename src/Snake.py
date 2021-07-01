6ac2a36e98ca7a02aa1356dc3e4163200405072a
Matt Murphy <mmurph56@binghamton.edu>
2021-07-01 16:35:30 -0400

    added snake image files

--- src/Snake.py
+++ src/Snake.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-class Segement(pygame.sprite.Sprite):
+class Segment(pygame.sprite.Sprite):
     def __init__(self, x, y, img_file):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img_file).convert_alpha()
